68186f2a13880b360de2ad5fa4e450b5171a11b4
fujioka8700 <fujioka8700@gmail.com>
2020-12-26 14:47:39 +0900

    スライダーと、ハンバーガーメニューのプログラム、追加しました。

---
+++ js/script.js
@@ -0,0 +1,30 @@
+'usestrict';
+
+//ハンバーガーメニュー
+function toggleNav() {
+    var body = document.body;
+    var hamburger = document.getElementById('js-hamburger');
+    var blackBg = document.getElementById('js-black-bg');
+
+    hamburger.addEventListener('click', function () {
+        body.classList.toggle('nav-open');
+    });
+    blackBg.addEventListener('click', function () {
+        body.classList.remove('nav-open');
+    });
+}
+toggleNav();
+
+//slick-slider設定
+$('.slider').slick({
+    autoplay:true,
+    autoplaySpeed:5000,
+    dots:true,
+    slidesToShow:1,
+    slidesToScroll:1,
+    arrows: true,
+    prevArrow:'<div class="prev"><img src="images/sp_top-slider-prevbutton.png"></div>',
+    nextArrow:'<div class="next"><img src="images/sp_top-slider-nextbutton.png"></div>',
+  });
+
+  console.log($('.slider'));
